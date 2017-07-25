a45992ab651f55a4d0dd529eba67521d41dfd07e
Adriana Donkers <adriana.m.donkers@gmail.com>
2017-07-25 12:01:49 -0700

    hide and show subactivities FINALLY WORKSgit add . :D

--- javascript/plan.js
+++ javascript/plan.js
@@ -1,4 +1,4 @@
-//function shows and hides sub activity options under the plan page
+// function shows and hides sub activity options under the plan page
 function showOptions(topic) {
      var x = document.getElementById(topic);
      if (x.style.display === 'block') {
