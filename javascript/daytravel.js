16356ea0852520c3ba2f626186f97a11c9ec0acb
Thomas <thomaszhang046@gmail.com>
2017-07-28 11:34:27 -0700

    LOLOLOLOL

--- javascript/daytravel.js
+++ javascript/daytravel.js
@@ -1,8 +1,20 @@
 function address () {
 console.log("address")
 }
+var options = {
+  types: ['(cities)'],
+  componentRestrictions: {country: "us"}
+ };
 
-var autocomplete = new google.maps.places.Autocomplete($("#pac-input")[0]);
+ var input = document.getElementById('pac-input');
+ var autocomplete = new google.maps.places.Autocomplete(input, options);
+
 
 
  autocomplete.addListener('place_changed', address);
+
+function next2Click() {
+  var city = $('#pac-input').val();
+  window.location = '/plan?city=' + city
+
+}
